54f70c8cd0c87f5cb57cab9c7600317cbf683ba7
AYOUB KHABALI <Ayoubkhabali9@gmail.com>
2024-11-20 18:35:47 +0100

    styling forms

--- DataFlowDesk/forms.py
+++ DataFlowDesk/forms.py
@@ -8,8 +8,34 @@ DATA_TYPES = [
     ('time', 'Time'),
 ]
 
+from django import forms
+
 class DatasetMetaForm(forms.Form):
-    name = forms.CharField(max_length=255, label="Dataset Name")
-    description = forms.CharField(widget=forms.Textarea, label="Description")
-    num_columns = forms.IntegerField(label="Number of Columns")
-    num_rows = forms.IntegerField(label="Number of Rows")
+    name = forms.CharField(
+        max_length=255, 
+        label="Dataset Name", 
+        widget=forms.TextInput(attrs={
+            'class': 'mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+        })
+    )
+    
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+        }), 
+        label="Description"
+    )
+    
+    num_columns = forms.IntegerField(
+        label="Number of Columns", 
+        widget=forms.NumberInput(attrs={
+            'class': 'mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+        })
+    )
+    
+    num_rows = forms.IntegerField(
+        label="Number of Rows", 
+        widget=forms.NumberInput(attrs={
+            'class': 'mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
+        })
+    )
