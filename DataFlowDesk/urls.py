6f1d1b1fc9b73b735d289e6339ec3803540d2d82
Andrew Kiswaga John <148845734+Andrew-Kiswaga-John@users.noreply.github.com>
2024-11-21 10:33:08 +0100

    changed the url

--- DataFlowDesk/urls.py
+++ DataFlowDesk/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('upload-file/', views.upload_file, name='upload_file'),
     path('create-dataset/step1/', views.create_dataset_step1, name='create_dataset_step1'),
     path('create-dataset/step2/', views.create_dataset_step2, name='create_dataset_step2'),
-    path('upload/', views.upload_file, name='upload_file'),
+    # path('upload/', views.upload_file, name='upload_file'),
 
 ]
 
